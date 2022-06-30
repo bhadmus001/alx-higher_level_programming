241489c8afd773e03166d8c11c69c859e6477602
Badmus Abdulmuhidh <Badmusmuhiz@gmail.com>
2022-06-30 23:13:07 +0000

    importing modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import sys
+if __name__ == '__name__':
+    import sys
 
-arg = len(sys.argv[1::])
-for i in range(1, arg + 1):
-    if arg == 1 and i == 1:
-        print("{} argument".format(arg))
-    elif arg >= 2 and i == 1:
-        print("{} arguments".format(arg))
-    print("{}: {}".format(i, sys.argv[i]))
+    arg = len(sys.argv[1::])
+    for i in range(1, arg + 1):
+        if arg == 1 and i == 1:
+            print("{} argument".format(arg))
+        elif arg >= 2 and i == 1:
+            print("{} arguments".format(arg))
+        print("{}: {}".format(i, sys.argv[i]))
