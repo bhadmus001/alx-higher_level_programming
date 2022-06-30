20c706830105097cf908aebc7ead14e204c0b71f
Badmus Abdulmuhidh <Badmusmuhiz@gmail.com>
2022-06-30 21:50:27 +0000

    importing modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,13 +1,2 @@
 #!/usr/bin/python3
-from add_0 import add
-
-
-def main():
-    a = 1
-    b = 2
-    sum1 = add(1, 2)
-    print("{:d} + {:d} = {:d}".format(a, b, sum1))
-
-
-if __name__ == '__main__':
-    main()
+import add_0
--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,10 +2,10 @@
 from calculator_1 import add, sub, mul, div
 
 
+if __name__ == '__main__':
 a = 10
 b = 5
-if __name__ == '__main__':
-    print("{:d} + {:d} = {:d}".format(a, b, add(10, 5)))
-    print("{:d} - {:d} = {:d}".format(a, b, sub(10, 5)))
-    print("{:d} * {:d} = {:d}".format(a, b, mul(10, 5)))
-    print("{:d} / {:d} = {:.0f}".format(a, b, div(10, 5)))
+    print("{} + {} = {}".format(a, b, add(10, 5)))
+    print("{} - {} = {}".format(a, b, sub(10, 5)))
+    print("{} * {} = {}".format(a, b, mul(10, 5)))
+    print("{} / {} = {}".format(a, b, div(10, 5)))
