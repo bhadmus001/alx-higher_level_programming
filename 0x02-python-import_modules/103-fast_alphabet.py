ca0f947f56450fcf68e62f8e86063dec98c13750
Badmus Abdulmuhidh <Badmusmuhiz@gmail.com>
2022-07-01 01:24:17 +0000

    importing modules

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-from fast_alphabet_103 import print_alpha
-print_alpha()
+import fast_alphabet_103 
--- 0x02-python-import_modules/fast_alphabet_103.py
+++ 0x02-python-import_modules/fast_alphabet_103.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def print_alpha():
-    for i in range(65, 91):
-        print("{}".format(chr(i)), end='')
-    print('\n')
+for i in range(65, 91):
+    print("{}".format(chr(i)), end='')
+print('\n')
