84751b9f41b4fe4aab8e9edf8166f816db65e50c
Badmus Abdulmuhidh <Badmusmuhiz@gmail.com>
2022-07-25 22:41:43 +0000

    More programs on classes using python

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" empty class Rectangle that defines a rectangle"""
+
+
 class Rectangle:
     """ class rectangle"""
     def __init__(self, width=0, height=0):
--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" empty class Rectangle that defines arectangle """
+
+
 class Rectangle:
     """ class rectangle """
     def __init__(self, width=0, height=0):
