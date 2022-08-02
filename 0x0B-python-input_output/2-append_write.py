4c27e681fec0be8f1655478f270e7d02fa109f22
Badmus Abdulmuhidh <Badmusmuhiz@gmail.com>
2022-08-02 22:45:58 +0000

    Input and output of files in python

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Module that contains a function that appends to the end of a text file """
+
+
+def append_write(filename="", text=""):
+    """ Function that appends to the end of a text file and returns the number of characters appended
+    Args:
+        filename: filename
+        text: text to append
+    Raises
+        Exception: when the file can be opened
+    """
+    with open(filename, 'a', encoding='utf-8') as f:
+        return f.write(text)
