78f8f1177cd03bcdb95edddf98298e9cc1435fc2
Badmus Abdulmuhidh <Badmusmuhiz@gmail.com>
2022-08-06 22:19:44 +0000

    Exception handling

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,8 +3,8 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             print("{}".format(my_list[i]), end='')
-        print()
     except IndexError as e:
         print()
-    finally:
-        return i + 1
+        return i
+    print()
+    return i + 1
