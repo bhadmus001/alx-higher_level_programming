fc2299e51ed835982d3ad9eb53927670044ad84e
Badmus Abdulmuhidh <Badmusmuhiz@gmail.com>
2022-08-07 00:32:30 +0000

    Exception handling

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
             y += 1
         except (ValueError, TypeError):
             continue
-        except IndexError as e:
-            break
+        '''except IndexError as e:
+            break'''
     print()
     return y
