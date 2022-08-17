a9ae4899296840d0c5d64a4e8a0c61f53fe3b052
Badmus Abdulmuhidh <Badmusmuhiz@gmail.com>
2022-08-17 10:56:54 +0000

    OOP classes using python

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -18,7 +18,7 @@ class Square:
             None
         """
         if type(size) is not int:
-            raise TypeError('size must be integer')
+            raise TypeError('size must be an integer')
         elif size < 0:
             raise ValueError('size must be >= 0')
         else:
