5477dd0266e4817f3b07024ab3dfc0eb120f269a
Badmus Abdulmuhidh <Badmusmuhiz@gmail.com>
2022-08-30 01:53:14 +0000

    Concept of Inheritance in OOP using Ptython

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""A function that returns the list of available attributes \n
+and methods of an object"""
+
+
 def lookup(obj):
     """Returns the list of available attributes and methods of an object"""
     return dir(obj)
--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """A class that inherits directly from the list class"""
+
+
 class MyList(list):
     """Inherits from the list class"""
     def print_sorted(self):
